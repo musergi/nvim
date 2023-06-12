68e197dcc9e06167cf23e25b8957288479cfcaf6
musergi <sergi.mus@gmail.com>
2023-06-12 21:57:40 +0200

    Updated with theme and LSP

--- init.lua
+++ init.lua
@@ -1,16 +1,1 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
-
-vim.opt.background = 'dark'
-
-
-require('plugin_load')
-
-require('nvim-tree').setup()
-require('telescope').setup()
+require('musergi')
---
+++ lua/musergi/packer.lua
@@ -0,0 +1,42 @@
+return require('packer').startup(function(use)
+    use('wbthomason/packer.nvim')
+
+    use({
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = {
+            { 'nvim-lua/plenary.nvim' }
+        }
+    })
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+
+    use("rebelot/kanagawa.nvim")
+end)
